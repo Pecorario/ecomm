5de82bc4a759abe83f8b3d3db428a36c5d20a886
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-06-14 17:54:25 -0300

    :sparkles: add find category by id

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -8,6 +8,18 @@ class CategoryService {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  static async findCategoryById(id) {
+    try {
+      const response = await fetch('http://localhost:3000/categories');
+      console.log('response status: ', response.status)
+      const json = await response.json();
+      const category = json.find(item => item.id == id);
+      console.log(category);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -4,10 +4,14 @@ const args = process.argv;
 
 async function processarComando(args) {
   const command = args[2];
+  const id = args[3];
 
   switch (command) {
     case '--listarCategorias':
       CategoryService.findCategories();
+      break;
+    case '--recuperarCategoriaPorId':
+      CategoryService.findCategoryById(id);
       break;
     default:
       return 'comando não encontrado';
