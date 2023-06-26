936eae118bf8bf293a02e8cb52cebfdbc4df127e
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-06-25 21:32:04 -0300

    :sparkles: add delete and activate category by id

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -7,5 +7,7 @@ router.get('/api/categories', CategoriesController.getAllCategories);
 router.post('/api/admin/categories', CategoriesController.addNewCategory);
 router.get('/api/categories/:id', CategoriesController.getCategoryById);
 router.put('/api/admin/categories/:id', CategoriesController.updateCategoryById);
+router.patch('/api/admin/categories/:id', CategoriesController.activateCategoryById);
+router.delete('/api/admin/categories/:id', CategoriesController.deleteCategoryById);
 
 export default router;
