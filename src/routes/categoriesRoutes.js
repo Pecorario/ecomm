69514008b8a452849d5f7751f1b666db748d4453
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-06-25 21:30:19 -0300

    :sparkles: add update category by id

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/api/categories', CategoriesController.getAllCategories);
 router.post('/api/admin/categories', CategoriesController.addNewCategory);
 router.get('/api/categories/:id', CategoriesController.getCategoryById);
+router.put('/api/admin/categories/:id', CategoriesController.updateCategoryById);
 
 export default router;
